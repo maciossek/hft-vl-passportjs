dff292440d8f661325accb6116156b422fd00693
Mathias Maciossek <mathias.maciossek@gmail.com>
2023-04-16 18:47:06 +0200

    🎉 Inital commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Notes from "reveal.js/plugin/notes/notes.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/white.css";
+import "./default.css";
+import "./atom-one-dark.css";
+import RevealMermaid from "reveal.js-mermaid-plugin";
+
+let deck = new Reveal({
+  plugins: [Markdown, Highlight, Notes, RevealMermaid],
+});
+deck.initialize({
+  controls: true,
+  progress: true,
+  hash: true,
+  history: true,
+});
